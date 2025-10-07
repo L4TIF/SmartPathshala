5b615f502c588b640d2a8bb3bf9474a9cfb4f97b
kajalkumari-114 <kajalkumarijii2003@gmail.com>
2025-10-08 04:54:54 +0530

    add faq

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Intro from '../components/home/intro'
-import Hero from '../components/home/Hero'
-import Achievements from '../components/home/Achievements'
-import FAQ from '../components/home/FAQ'
-
+import React from "react";
+import Intro from "../components/home/intro";
+import Hero from "../components/home/Hero";
+import Achievements from "../components/home/Achievements";
+import FAQ from "../components/home/FAQ";
 
 const Home = () => {
   return (
     <>
       <Intro />
       <Hero />
-      <div><Achievements /></div>
-      <div><FAQ /></div>
-      <div><Footer /></div>
-
+      <div>
+        <Achievements />
+      </div>
+      <div>
+        <FAQ />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
